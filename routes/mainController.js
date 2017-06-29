d46002564bc52012887849dc9347f9416b94e2b7
song suin <suinsong92@gmail.com>
2017-06-29 12:03:49 +0900

    first commit

---
+++ routes/mainController.js
@@ -0,0 +1,64 @@
+module.exports = function(app, stVO) {
+
+	app.get('/insert', function(req, res) {
+		var vo = stVO
+		res.render('input_form', {
+			item : vo,
+			action : '/insert',
+			pageTitle : '등록'
+		});
+	})
+
+	app.post('/insert', function(req, res) {
+		var vo = new stVO(req.body);
+		vo.save(function(err, data) {
+			res.json(data);
+		})
+	})
+
+	app.get('/list', function(req, res) {
+		stVO.find(function(err, data) {
+			// res.json(data);
+			res.render('list', {
+				list : data
+			})
+		})
+	})
+	
+
+	
+	app.get('/delete/:id', function(req, res) {
+		var id = req.params.id
+		stVO.remove({
+			_id : id
+		}, function(err, data) {
+			res.redirect('/list')
+		})
+	})
+
+	app.get('/update/:id', function(req, res) {
+		var id = req.params.id
+		stVO.findOne({
+			_id : id
+		}, function(err, data) {
+			res.render('input_form', {
+				item : data,
+				pageTitle : '수정',
+				action : '/update'
+			})
+
+		})
+	})
+
+	app.post('/update', function(req, res) {
+		var id = req.body.id;
+		stVO.update({
+			_id : id
+		}, {
+			$set : req.body
+		}, function(err, data) {
+			res.redirect('/list')
+		})
+	})
+
+}
